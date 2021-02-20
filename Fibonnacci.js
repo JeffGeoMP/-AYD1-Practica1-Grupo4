ca3b729e9c6149a439d9ae556e7ffacbbcd0eb6c
Hayrton <2172182021503@ingenieria.usac.edu.gt>
2021-02-20 17:21:23 -0600

    201313875 - Se agrego funcion para saber numero primo

--- Fibonnacci.js
+++ Fibonnacci.js
@@ -7,3 +7,17 @@ function fibonacci(n) {
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+
+
+/*Numero primo*/
+var num = parseInt(prompt("Ingrese el numero"));
+var i=1;
+var cont=0;
+
+while(i<=num-1){
+    if(num%i==0){cont= cont+1;}
+    i++;
+}
+if(cont==1){console.log(num," Si es primo");}
+else{console.log(num," no es primo");}
