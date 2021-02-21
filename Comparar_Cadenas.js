7047ae234ec17aafee546ced36d67ef04e3cce5c
Alejandro Marín <cheealejoomariin@gmail.com>
2021-02-20 22:15:34 -0600

    201602855->Comparacion entre dos cadenas

---
+++ Comparar_Cadenas.js
@@ -0,0 +1,10 @@
+// José Alejandro Grande Marín 201602855
+var op1= prompt("Cadena_1");
+var op2 =prompt("cadena_2");
+
+if(op1==op2){
+alert("Las cadenas SI son iguales");
+}else{
+alert("Las cadenas NO son iguales");
+
+}
